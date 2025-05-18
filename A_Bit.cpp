49f46e6e692adaac07ad528c5c8d63063960a3a6
Kaarti22 <kaartimondi@gmail.com>
2025-05-18 17:23:05 +0530

    Bit++

---
+++ A_Bit.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define MOD 1000000007
+#define pb push_back
+#define popb pop_back
+#define rep(i,a,b) for(int i=a; i<b; i++)
+#define all(v) v.begin(),v.end()
+
+void inparr(int arr[], int n){
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+}
+
+void disarr(int arr[], int n){
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<' ';
+    }
+    cout<<endl;
+}
+
+void inpvec(vi &v, int n){
+    for(int i=0;i<n;i++){
+        cin>>v[i];
+    }
+}
+
+void disvec(vi v, int n){
+    for(int i=0;i<n;i++){
+        cout<<v[i]<<' ';
+    }
+    cout<<endl;
+}
+
+int madd(int a,int b) {
+    return (a+b)%MOD;
+}
+
+int msub(int a,int b){
+    return (((a-b)%MOD)+MOD)%MOD;
+}
+
+int mmul(int a,int b){
+    return ((a%MOD)*(b%MOD))%MOD;
+}
+
+class Kaarti{
+public:
+    void method(int n, vector<string>& v){
+        int x = 0;
+        rep(i,0,n){
+            int plus = 0, minus = 0;
+            for(int j=0; j<3; j++){
+                if(v[i][j] == '+') plus++;
+                if(v[i][j] == '-') minus++;
+            }
+            if(plus == 2) x++;
+            else x--;
+        }
+        cout<<x<<endl;
+    }
+};
+
+int32_t main(){
+    int n;
+    cin>>n;
+    vector<string> v(n);
+    rep(i,0,n){
+        cin>>v[i];
+    }
+    Kaarti ob;
+    ob.method(n, v);
+    return 0;
+}
