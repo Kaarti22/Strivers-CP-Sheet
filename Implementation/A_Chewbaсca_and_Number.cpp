468369b611d2ec904eade5cf6c1b8c07787f9622
Kaarti22 <kaartimondi@gmail.com>
2025-05-18 17:42:51 +0530

    Chewbacca and Number

---
+++ Implementation/A_Chewbaсca_and_Number.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define MOD 1000000007
+#define pb push_back
+#define popb pop_back
+#define rep(i,a,b) for(int i=a; i<b; i++)
+#define all(v) v.begin(),v.end()
+
+void inparr(int arr[], int n){
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+}
+
+void disarr(int arr[], int n){
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<' ';
+    }
+    cout<<endl;
+}
+
+void inpvec(vi &v, int n){
+    for(int i=0;i<n;i++){
+        cin>>v[i];
+    }
+}
+
+void disvec(vi v, int n){
+    for(int i=0;i<n;i++){
+        cout<<v[i]<<' ';
+    }
+    cout<<endl;
+}
+
+int madd(int a,int b) {
+    return (a+b)%MOD;
+}
+
+int msub(int a,int b){
+    return (((a-b)%MOD)+MOD)%MOD;
+}
+
+int mmul(int a,int b){
+    return ((a%MOD)*(b%MOD))%MOD;
+}
+
+class Kaarti{
+public:
+    void method(int n){
+        string s = to_string(n);
+        int len = s.size();
+        rep(i,0,len){
+            int digit = s[i] - '0';
+            int inv = 9 - digit;
+            if(i == 0 && digit == 9) continue;
+            s[i] = min(digit, inv) + '0';
+        }
+        cout<<s<<endl;
+    }
+};
+
+int32_t main(){
+    int n;
+    cin>>n;
+    Kaarti ob;
+    ob.method(n);
+    return 0;
+}
