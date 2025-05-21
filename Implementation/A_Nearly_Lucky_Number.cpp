ac83ff5daec6dd47cc803d28f37c172249063a13
Kaarti22 <kaartimondi@gmail.com>
2025-05-21 23:42:54 +0530

    nearly lucky number

---
+++ Implementation/A_Nearly_Lucky_Number.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define MOD 1000000007
+#define pb push_back
+#define popb pop_back
+#define rep(i,a,b) for(int i=a; i<b; i++)
+#define all(v) v.begin(),v.end()
+
+void inparr(int arr[], int n){
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+}
+
+void disarr(int arr[], int n){
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<' ';
+    }
+    cout<<endl;
+}
+
+void inpvec(vi &v, int n){
+    for(int i=0;i<n;i++){
+        cin>>v[i];
+    }
+}
+
+void disvec(vi v, int n){
+    for(int i=0;i<n;i++){
+        cout<<v[i]<<' ';
+    }
+    cout<<endl;
+}
+
+int madd(int a,int b) {
+    return (a+b)%MOD;
+}
+
+int msub(int a,int b){
+    return (((a-b)%MOD)+MOD)%MOD;
+}
+
+int mmul(int a,int b){
+    return ((a%MOD)*(b%MOD))%MOD;
+}
+
+class Kaarti{
+public:
+    void method(int n){
+        int c = 0;
+        while(n > 0){
+            int digit = n % 10;
+            if(digit == 4 || digit == 7) c++;
+            n /= 10;
+        }
+        if(c == 4 || c == 7) cout<<"YES"<<endl;
+        else cout<<"NO"<<endl;
+    }
+};
+
+int32_t main(){
+    int n;
+    cin>>n;
+    Kaarti ob;
+    ob.method(n);
+    return 0;
+}
