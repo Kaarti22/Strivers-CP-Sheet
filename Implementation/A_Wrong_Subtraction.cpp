fd313aee7530666cde0fe586f8ade28671932f40
Kaarti22 <kaartimondi@gmail.com>
2025-05-22 10:20:33 +0530

    wrong subtraction

---
+++ Implementation/A_Wrong_Subtraction.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define MOD 1000000007
+#define pb push_back
+#define popb pop_back
+#define rep(i,a,b) for(int i=a; i<b; i++)
+#define all(v) v.begin(),v.end()
+
+void inparr(int arr[], int n){
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+}
+
+void disarr(int arr[], int n){
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<' ';
+    }
+    cout<<endl;
+}
+
+void inpvec(vi &v, int n){
+    for(int i=0;i<n;i++){
+        cin>>v[i];
+    }
+}
+
+void disvec(vi v, int n){
+    for(int i=0;i<n;i++){
+        cout<<v[i]<<' ';
+    }
+    cout<<endl;
+}
+
+int madd(int a,int b) {
+    return (a+b)%MOD;
+}
+
+int msub(int a,int b){
+    return (((a-b)%MOD)+MOD)%MOD;
+}
+
+int mmul(int a,int b){
+    return ((a%MOD)*(b%MOD))%MOD;
+}
+
+class Kaarti{
+public:
+    void method(int n, int k){
+        while(k--){
+            int digit = n % 10;
+            if(digit == 0) n /= 10;
+            else n--;
+        }
+        cout<<n<<endl;
+    }
+};
+
+int32_t main(){
+    int n, k;
+    cin>>n>>k;
+    Kaarti ob;
+    ob.method(n, k);
+    return 0;
+}
