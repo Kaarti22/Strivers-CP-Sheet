a044aa02999a03c566824132787fb713056fcb3d
Kaarti22 <kaartimondi@gmail.com>
2025-05-31 00:47:19 +0530

    erasing zeroes

---
+++ Implementation/A_Erasing_Zeroes.cpp
@@ -0,0 +1,94 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define MOD 1000000007
+#define pb push_back
+#define popb pop_back
+#define rep(i,a,b) for(int i=a; i<b; i++)
+#define all(v) v.begin(),v.end()
+
+void inparr(int arr[], int n){
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+}
+
+void disarr(int arr[], int n){
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<' ';
+    }
+    cout<<endl;
+}
+
+void inpvec(vi &v, int n){
+    for(int i=0;i<n;i++){
+        cin>>v[i];
+    }
+}
+
+void disvec(vi v, int n){
+    for(int i=0;i<n;i++){
+        cout<<v[i]<<' ';
+    }
+    cout<<endl;
+}
+
+int madd(int a,int b) {
+    return (a+b)%MOD;
+}
+
+int msub(int a,int b){
+    return (((a-b)%MOD)+MOD)%MOD;
+}
+
+int mmul(int a,int b){
+    return ((a%MOD)*(b%MOD))%MOD;
+}
+
+class Kaarti{
+public:
+    void method(string& s){
+        int n = s.size();
+        int st = -1, end = -1;
+        rep(i,0,n){
+            if(s[i] == '1'){
+                st = i;
+                break;
+            }
+        }
+        if(st == -1){
+            cout<<0<<endl;
+            return;
+        }
+        for(int i=n-1; i>=0; i--){
+            if(s[i] == '1'){
+                end = i;
+                break;
+            }
+        }
+        int ans = 0;
+        rep(i,st,end){
+            if(s[i] == '0'){
+                ans++;
+            }
+        }
+        cout<<ans<<endl;
+    }
+};
+
+int32_t main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int testcase;
+    cin>>testcase;
+    for(int t=0; t<testcase; t++){
+        string s;
+        cin>>s;
+        Kaarti ob;
+        ob.method(s);
+    }
+    return 0;
+}
